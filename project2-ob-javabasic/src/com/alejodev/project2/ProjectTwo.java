f533f25b6df28a5f525ca85022211c6962f9ab10
Alejandro Alonso <brayan58alonso@gmail.com>
2022-09-14 10:41:32 -0500

    Updating projects

---
+++ project2-ob-javabasic/src/com/alejodev/project2/ProjectTwo.java
@@ -0,0 +1,16 @@
+package com.alejodev.project2;
+
+public class ProjectTwo {
+
+    private static double iva= 0.19;
+
+    public static void main(String[] args) {
+
+        double resultado = getprice(15.98);
+        System.out.println(resultado);
+
+    }
+    public static double getprice(double price) {
+        return price + (price * iva);
+    }
+}
