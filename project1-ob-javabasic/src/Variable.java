3fb5cf1e9b0fbfe2160e1ab34918ab9e30dfcb6f
Alejandro Alonso <brayan58alonso@gmail.com>
2022-09-14 10:14:38 -0500

    Updating projects

---
+++ project1-ob-javabasic/src/Variable.java
@@ -0,0 +1,53 @@
+package firstSession;
+
+public class Variable {
+
+    public static void main(String[] args) {
+
+        // numericos
+        //Enteros
+
+        byte variable1 = 2;
+        System.out.println ( variable1 ) ;
+
+        short variable2 = 10;
+        System.out.println ( variable2 ) ;
+
+        int variable3 = 30 ;
+        System.out.println ( variable3 ) ;
+
+        long variable4 = 100 ;
+        System.out.println ( variable4 ) ;
+
+        Long variable5 = 666555444L;
+        System.out.println ( variable5 ) ;
+
+
+        // 1.2 decimales
+
+        float variable6 = 5.5f ;
+        System.out.println ( variable6 ) ;
+
+        double variable7 = 10.5d ;
+        System.out.println ( variable7 ) ;
+
+
+        // 2. booleano
+        boolean variable8 = false ;
+        System.out.println ( variable8 ) ;
+
+        boolean variable9 = true ;
+        System.out.println ( variable9 ) ;
+
+
+
+        // 3. texto
+        char variable10 = 'a' ;
+        System.out.println ( variable10 ) ;
+
+        String variable11 = " Lorem ipsum dolor sit amet ..... " ;
+        System.out.println ( variable11 ) ;
+
+
+    }
+}
